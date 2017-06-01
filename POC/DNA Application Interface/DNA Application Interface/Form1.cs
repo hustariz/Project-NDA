0cbf47ee3ce35a2689bb95a5f3e5f39670441681
FizzyMaki <hugo.ustariz@gmail.com>
2017-06-01 18:24:03 +0200

    Ajout du poc windform connexion socket

---
+++ POC/DNA Application Interface/DNA Application Interface/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace DNA_Application_Interface
+{
+    public partial class Form1 : Form
+    {
+
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public String receive;
+        public String text_to_send;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());  // get my own IP
+            foreach(IPAddress address in localIP)
+            {
+                if(address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textBox3.Text = address.ToString();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Start Server
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(textBox4.Text));
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+
+            backgroundWorker1.RunWorkerAsync(); // Start receiving Data in background.
+            backgroundWorker2.WorkerSupportsCancellation = true; // Ability to cancel this tread.
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) // receive data
+        {
+
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e) // send data
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Connect to Server
+        {
+            client = new TcpClient();
+        }
+    }
+}
